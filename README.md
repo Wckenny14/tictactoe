11770f5f091b392ef5923bad82392c871032bec5
Wayne Kenny <waynekenny@waynes-air.home>
2022-12-22 20:14:33 +1100

    nearly there

--- README.md
+++ README.md
@@ -1,32 +1,96 @@
-# Wayne's TicTacToe
+# My first project: Tic-Tac-Toe
 
-
-Plain text just gets imported as part of the paragraph
-
-Having a line break `from the above text` will create a seperate paragraph otherwise it will all sit on the same line
-
-## Heading 2
-### Heading 3
-
-- List item 1 - this is how to create a ul list
-- List item 2
-- List item 3
-
-1. Item 1 - This is how to create an ol list
-2. Item 2
-3. Item 3
-
-```Javascript
-for (let block of blocks) {
-    block.addEventListner('click, handleClick, {once: true}');
-}
-```
-can do the same as above for HTML and CSS
-
-![doggie](https//)
-
+I recently became a student at General Assembly (GA) in Sydney, Australia studying Software Engineering in a full time immersive course and this is my first student project.
 
 ---
+
+
+### Brief
+
+- Build a web application from scratch
+- Map out the game logic
+- Create seperate HMTL, CSS and JavaScript files in the application
+- Build a dynamic game that allows two players to compete
+
+---
+
+
+### Technical Requirements
+
+- Render the game board in the browser
+- Switch turns between chosen markers
+- Visually display a win or draw
+
+---
+
+
+### Planning Stage
+
+I wrote my initial HTML tree in excel as this was the program I was most familiar with.
+
+This allowed me to see how elements were linked within the tree and where I might require JS.
+
+![Plan_in_excel](/tictactoe/Screenshot%202022-12-18%20at%208.05.08%20pm.png))
+
+I also tried to write out my logic in word to help guide me through the process of writing the code but it turned out to be a real mess and completely wrong so I have chosen to omit it from this document.
+
+---
+
+
+### Writing The Code
+
+At the point of starting to write the code I had only gone through around 20 hours of studying JavaScript and this was my total hours of experience so I wasn't really sure what I was doing.
+This was the code that I wrote before I found myself at a loss of where to go next:
+
+```Javascript
+// Access board elements in the DOM
+const playerTurn = document.querySelector(".turn")
+const result = document.querySelector(".result")
+const reset = document.querySelector(".reset")
+
+// Accessing the 'board' in the DOM and converting it to an Array
+const boardSquares = Array.from(document.querySelectorAll(".square"));
+
+// Create players markers and current player
+const playerOne = 'X';
+const playerTwo = 'O';
+let currentPlayer = playerOne
+```
+
+When I found myself at a loss I did what I understand all seasoned developers do and turned to google for answers. What i found at first probably confused me more than helped but I would like to says thanks to the following video which helped get me started:
+
+- https://www.youtube.com/watch?v=P2TcQ3h0ipQ&t=591s
+
+I was conscious of not directly copying anything so I used this for inspiration and tried to do my own thing.
+
+---
+
+### Bottlenecks and Challenges
+
+To say this wasn't a challenge would be a lie. I often found myself scratching my head wondering what to do next and looking to google to provide the answers which 70% of the time didn't seem to be there.
+
+Luckily in the end I did manage to overcome all hurdles to deliver this project but I would like to highlight one bit of cose that had me stumped for a whole day.
+
+initially I planned to use a `for of loop` to go through the below winning combinations:
+
+```Javascript
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 6]
+]
+```
+
+Unfortunately this proved to be outside of my capabilities at the time and I ended up with something much less appealing to the eye but got the job done.
+
+---
+
+### Finished product
 
 
 Copyright Wayne Kenny
--- ttt_script.js
+++ ttt_script.js
@@ -3,31 +3,34 @@
 // Access board elements in the DOM
 const playerTurn = document.querySelector(".turn")
 const result = document.querySelector(".result")
+const reset = document.querySelector(".reset")
 
 // Accessing the 'board' in the DOM and converting it to an Array
 const boardSquares = Array.from(document.querySelectorAll(".square"));
-console.log(boardSquares)
 
 // Create players markers and current player
 const playerOne = 'X';
 const playerTwo = 'O';
 let currentPlayer = playerOne
 
+// Create variable to begin counting through the board after a players turn
+let count = 0;
+
 // Create winning combos
-const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 4, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 4, 6]
-]
+// const winCombos = [
+//     [0, 1, 2],
+    // [3, 4, 5],
+    // [6, 7, 8],
+    // [0, 4, 8],
+    // [0, 3, 6],
+    // [1, 4, 7],
+    // [2, 5, 8],
+    // [3, 4, 6]
+// ]
 
-////////////////// Start the game
-const startGame = function () {
-
+////////////////// Running the game
+const startGame = function() {
+    
     //loop through the board when a player clicks on a Square
     boardSquares.forEach(function(square) {
         function clickedSquare(square) {
@@ -38,37 +41,77 @@ const startGame = function () {
 
             // set player turn and switch between players
             turn(square.target.id, currentPlayer)
-            
             if (currentPlayer === playerOne) {
-                currentPlayer = playerTwo
+                currentPlayer = playerTwo;
+                playerTurn.innerHTML = "Player 2's turn";
             } else {
-                currentPlayer = playerOne
+                currentPlayer = playerOne;
+                playerTurn.innerHTML = "Player 1's turn"
             }
-
-
+            count++;
+            console.log(count)
+            checkWinner();
         }
         //Create the turn function that places the player marker in a particular square
         function turn(squareId, player) {
-            boardSquares[squareId] = player;
             document.querySelector(`#${squareId}`).innerText = player
         }
         // create event where a click on the square runs through the above functions to place a marker on a square
         square.addEventListener('click', clickedSquare)
     })   
 }
-// call the start game function
+///////////// Check for winner
+function checkWinner() {
+    if ((boardSquares[0].innerText === playerOne && boardSquares[1].innerText === playerOne && boardSquares[2].innerText === playerOne) || 
+        (boardSquares[3].innerText === playerOne && boardSquares[4].innerText === playerOne && boardSquares[5].innerText === playerOne) || 
+        (boardSquares[6].innerText === playerOne && boardSquares[7].innerText === playerOne && boardSquares[8].innerText === playerOne) || 
+        (boardSquares[0].innerText === playerOne && boardSquares[3].innerText === playerOne && boardSquares[6].innerText === playerOne) || 
+        (boardSquares[1].innerText === playerOne && boardSquares[4].innerText === playerOne && boardSquares[7].innerText === playerOne) || 
+        (boardSquares[2].innerText === playerOne && boardSquares[5].innerText === playerOne && boardSquares[8].innerText === playerOne) || 
+        (boardSquares[0].innerText === playerOne && boardSquares[4].innerText === playerOne && boardSquares[8].innerText === playerOne) || 
+        (boardSquares[2].innerText === playerOne && boardSquares[4].innerText === playerOne && boardSquares[6].innerText === playerOne)) {
+            result.innerHTML = 'Player 1 Wins!';
+            result.style.display = 'flex'
+    } else if ((boardSquares[0].innerText === playerTwo && boardSquares[1].innerText === playerTwo && boardSquares[2].innerText === playerTwo) || 
+        (boardSquares[3].innerText === playerTwo && boardSquares[4].innerText === playerTwo && boardSquares[5].innerText === playerTwo) || 
+        (boardSquares[6].innerText === playerTwo && boardSquares[7].innerText === playerTwo && boardSquares[8].innerText === playerTwo) || 
+        (boardSquares[0].innerText === playerTwo && boardSquares[3].innerText === playerTwo && boardSquares[6].innerText === playerTwo) || 
+        (boardSquares[1].innerText === playerTwo && boardSquares[4].innerText === playerTwo && boardSquares[7].innerText === playerTwo) || 
+        (boardSquares[2].innerText === playerTwo && boardSquares[5].innerText === playerTwo && boardSquares[8].innerText === playerTwo) || 
+        (boardSquares[0].innerText === playerTwo && boardSquares[4].innerText === playerTwo && boardSquares[8].innerText === playerTwo) || 
+        (boardSquares[2].innerText === playerTwo && boardSquares[4].innerText === playerTwo && boardSquares[6].innerText === playerTwo)) {
+            result.innerHTML = 'Player 2 Wins!';
+            result.style.display = 'flex'
+    } else
+        // check for a tie - all squares filled and no winner
+        // console.log('if tie', count)
+        if (count == 9) {
+            result.innerHTML = 'Tie!';
+            result.style.display = 'flex'
+            // return;
+        }
+    }
+
+//////// Stretch goal - Reset the game
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        boardSquares[i].innerText = ''
+    }
+        result.style.display = 'none';
+        playerTurn.innerText = "Player 1's turn";
+        currentPlayer = playerOne;
+        count = 0
+}
+// start game
 startGame()
 
 
-// run through winning combos
-
-
-// reset game
 
 
 
-// console.log(winCombos)
 
+
+///////// Alternate way of adding player marker to square and switching players
 
 //         // listen out for a click on a square in the board and take action when it happens
 //         square.addEventListener('click', function () {
